d1b58bef7fb9cb5c4a43b313c7e8ea93f3822efb
saihs0912 <h71534@gmail.com>
2025-09-05 01:09:00 +0800

    修改路徑

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/caffeineshop/dist/'
+  publicPath: '/caffeineshop/'
 })
