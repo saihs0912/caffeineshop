001eb859379857169495c465f0dc9d7c245b2d54
saihs0912 <h71534@gmail.com>
2025-09-05 00:57:49 +0800

    更新deploy

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,12 @@ npm run build
 # 移動至到打包後的dist目錄 
 cd dist
 
-# 因為dist資料夾預設是被ignore的，
-因此在進入dist資料夾後初始化git
+# 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git init 
 git add -A
 git commit -m 'deploy'
 
-# 將 dist資料夾中的內容推送至遠端 儲存庫的
-gh-pages分支中，
-並強制無條件將舊有的內容取代成目前的內容
-（指令 git push -f)
+# 將 dist資料夾中的內容推送至遠端 儲存庫的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 git push -f github網址 master:gh-pages
 cd -
 
