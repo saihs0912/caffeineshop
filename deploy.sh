93b464cb0c98adb4016a174c25d71c6a8abe660e
saihs0912 <h71534@gmail.com>
2025-09-05 01:03:05 +0800

    deploy二度更新

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git add -A
 git commit -m 'deploy'
 
 # 將 dist資料夾中的內容推送至遠端 儲存庫的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f github網址 master:gh-pages
+git push -f https://github.com/saihs0912/caffeineshop.git master:gh-pages
 cd -
 
 # 執行指令，在終端機輸入 deploy.sh
