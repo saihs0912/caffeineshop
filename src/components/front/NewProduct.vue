5c919658192815ccc2cc212b253f4f397621b577
saihs0912 <h71534@gmail.com>
2025-09-16 00:23:22 +0800

    首頁最新商品

--- src/components/front/NewProduct.vue
+++ src/components/front/NewProduct.vue
@@ -1,14 +1,25 @@
 <template>
+  <div class="areaBottom">
+    <h2 class="text-center"><span>新商品</span></h2>
     <div class="container-fluid">
         <div class="row">
-            <div class="col areaBottom">
-                <h2 class="text-center"><span>新進商品</span></h2>
-                <div class="d-grid gap-2 col-md-4 col-sm-4 col-6 mx-auto" style="padding: 20px 0;">
-                    <button class="btn btn-brown" type="button">查看全部商品</button>
+          <div v-for="item in latestProducts" :key="item.id" class="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div class="card h-100 shadow-sm">
+                <img :src="item.imageUrl" class="card-img-top" alt="商品圖片" />
+                <div class="card-body d-flex flex-column">
+                  <h5 class="card-title">{{ item.title }}</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <span class="fw-bold text-danger">{{ item.price }} 元</span>
+                  </div>
                 </div>
+              </div>
             </div>
         </div>
     </div>
+    <div class="d-grid gap-2 col-md-4 col-sm-4 col-6 mx-auto" style="padding: 20px 0;">
+      <button class="btn btn-brown" type="button">查看全部商品</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,12 +32,17 @@ export default {
   },
   methods: {
     getNewProduct () {
-      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products?page=1`
-      this.$http(url)
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`
+      this.$http.get(url)
         .then(res => {
           this.newProductList = res.data.products
           console.log(res.data)
         })
+    }
+  },
+  computed: {
+    latestProducts () {
+      return this.newProductList.slice(-3)
     }
   },
   created () {
