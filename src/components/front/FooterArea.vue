00273c22e003d3b3f5b0d4b4017d3ff923f794b3
saihs0912 <h71534@gmail.com>
2025-09-20 00:59:35 +0800

    商品詳細

--- src/components/front/FooterArea.vue
+++ src/components/front/FooterArea.vue
@@ -2,8 +2,18 @@
   <footer class="bg-light">
     <div class="container">
       <div class="row">
-        <div class="col">
-          <small>本網站僅供學習使用</small>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 p-4">
+          <div class="d-flex flex-nowrap align-items-center h-100">
+            <div>本網站僅供學習使用</div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 p-4">
+          <div class="d-flex flex-nowrap justify-content-end">
+            <div class="p-2"><i class="bi bi-facebook fs-2"></i></div>
+            <div class="p-2"><i class="bi bi-instagram fs-2"></i></div>
+            <div class="p-2"><i class="bi bi-threads fs-2"></i></div>
+            <div class="p-2"><i class="bi bi-envelope-fill fs-2"></i></div>
+          </div>
         </div>
       </div>
     </div>
--- src/views/front/ProductDetail.vue
+++ src/views/front/ProductDetail.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-9 col-lg-9 col-md-9 col-sm-9 col-xs-12">
-        <h2>{{ product.title }}</h2>
+      <div class="col-12">
+        <div class="mt-4 mb-4">
+          <span v-if="product.category === ('咖啡豆' || '掛耳咖啡包' || '咖啡生活用品')">咖啡</span>
+          <span v-else-if="product.category === ('罐裝茶' || '茶包' || '茶生活用品')">茶</span>
+          <span v-else>其他</span> > <span>{{ product.category }}</span>
+        </div>
       </div>
-      <div class="col3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <div class="col-9 col-lg-9 col-md-9 col-sm-9 col-xs-12 p-3">
+        <h2>{{ product.title }}</h2>
+        <div class="d-flex w-50">
+          <div class="w-25" v-for="(item, i) in  product.imagesUrl" :key="i"><img class="img-fluid" :src="product.imagesUrl[i]" alt=""></div>
+        </div>
+      </div>
+      <div class="col3 col-lg-3 col-md-3 col-sm-3 col-xs-12 p-3">
         {{ product.price }}元
       </div>
     </div>
