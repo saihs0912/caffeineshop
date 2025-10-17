fcde80f010e12b09bbc7efd4bccf42aa14bcb497
saihs0912 <h71534@gmail.com>
2025-10-18 01:42:34 +0800

    更新追蹤清單頁

--- src/views/front/FollowList.vue
+++ src/views/front/FollowList.vue
@@ -8,7 +8,31 @@
                 <h1>追蹤清單</h1>
                 <div class="container">
                     <div class="row">
-                        <div class="col-12"></div>
+                        <div class="col-12">
+                          <div>
+                            <table class="table">
+                              <thead>
+                                <tr>
+                                  <th></th>
+                                  <th>商品名稱</th>
+                                  <th>加入購物車/取消追蹤</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr v-for="(item, i) in filterData" :key="i">
+                                  <td><div style="width: 100px;"><img class="img-fluid" :src="filterData[i].imageUrl" alt=""></div></td>
+                                  <td>{{ item.title }}</td>
+                                  <td>
+                                    <div class="btn-group">
+                                      <button class="btn btn-outline-success btn-sm carts" type="button"></button>
+                                      <button class="btn btn-outline-danger btn-sm del" type="button"></button>
+                                    </div>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,6 +56,18 @@ export default {
         .then(res => {
           this.productList = res.data.products
         })
+    }
+  },
+  computed: {
+    filterData () {
+      const result = []
+      const num = this.favorite.length
+      this.productList.forEach(item => {
+        for (let i = 0; i < num; i++) {
+          if (item.id === this.favorite[i]) result.push(item)
+        }
+      })
+      return result
     }
   },
   created () {
