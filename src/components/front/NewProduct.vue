4f0819775e86f2b37d9a97690c95100572f99fab
saihs0912 <h71534@gmail.com>
2025-10-31 00:52:11 +0800

    完成首頁商品輪播

--- src/components/front/NewProduct.vue
+++ src/components/front/NewProduct.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="areaBottom mb-4">
     <h2 class="text-center"><span>新商品</span></h2>
-    <!-- <div class="container-fluid blockOut" style="padding: 20px 0!important;">
-      <div class="row blockIn">
-        <div v-for="(item, i) in latestProducts" :key="item.id" class="col-3 col-sm-3 col-md-3 col-lg-3 mb-3" :ref="el => boxesRefs[i] = el">
-          <div class="card h-100 shadow-sm newTag">
-            <router-link class="no-underline" :to="{ name: 'product', params: { productId: item.id } }">
-              <img :src="item.imageUrl" class="card-img-top" alt="商品圖片" />
-            </router-link>
-            <div class="card-body d-flex flex-column">
-              <router-link class="no-underline" :to="{ name: 'product', params: { productId: item.id } }">
-                <h5 class="card-title">{{ item.title }}</h5>
-              </router-link>
-              <div>
-                <span class="fw-bold text-danger">{{ item.price }} 元</span><br>
-                <span>{{ item.description }}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
     <div class="box-container">
-      <i class="bi bi-arrow-left-circle-fill icons i-l fs-1" @click="moveList(true)"></i>
+      <i v-if="x < 0" class="bi bi-arrow-left-circle-fill icons i-l text-brown" @click="moveList(true)"></i>
       <div class="box mx-auto my-3" :style="boxWidth">
         <div class="boxInside" ref="ProductBox" :style="boxInsideMove">
           <div class="card shadow-sm newTag m-2" v-for="item in latestProducts" :key="item.id">
@@ -41,7 +21,7 @@
           </div>
         </div>
       </div>
-      <i class="bi bi-arrow-right-circle-fill icons i-r fs-1" @click="moveList(false)"></i>
+      <i v-if="x > step*-4" class="bi bi-arrow-right-circle-fill icons i-r text-brown" @click="moveList(false)"></i>
     </div>
     <div class="d-grid gap-2 col-md-4 col-sm-4 col-6 mx-auto" style="padding: 20px 0;">
       <router-link class="btn btn-brown" to="/shopping">查看全部商品</router-link>
@@ -63,7 +43,9 @@ export default {
       boxInsideMove: {
         transform: 'translate3d(0, 0, 0)'
       },
-      x: 0
+      x: 0,
+      step: 0,
+      widthSize: ''
     }
   },
   methods: {
@@ -72,7 +54,6 @@ export default {
       this.$http.get(url)
         .then(res => {
           this.newProductList = res.data.products
-          console.log(res.data)
         })
     },
     setItemRef (el) {
@@ -80,20 +61,18 @@ export default {
       if (el) this.itemRefs.push(el)
     },
     moveList (arrow) {
-      const box = this.$refs.ProductBox
-      const step = box.clientWidth / 8
       if (arrow === true) {
         if (this.x === 0) {
           return false
         } else {
-          this.x += step
+          this.x += this.step
           this.boxInsideMove.transform = `translate3d(${this.x}px, 0, 0)`
         }
       } else if (arrow === false) {
-        if (this.x <= (step * -4)) {
+        if (this.x <= (this.step * -4)) {
           return false
         } else {
-          this.x -= step
+          this.x -= this.step
           this.boxInsideMove.transform = `translate3d(${this.x}px, 0, 0)`
         }
       }
@@ -104,16 +83,28 @@ export default {
       return this.newProductList.slice(-8).reverse()
     }
   },
-  created () {
+  watch: {
+    widthSize (newWidth, oldWidth) {
+      const box = this.$refs.ProductBox
+      let boxWidth = ''
+      if (newWidth >= 1200) {
+        boxWidth = (box.clientWidth / 8) * 4
+      } else if (newWidth <= 1199 && newWidth >= 992) {
+        boxWidth = (box.clientWidth / 8) * 3
+      } else if (newWidth <= 991 && newWidth >= 768) {
+        boxWidth = (box.clientWidth / 8) * 2
+      } else if (newWidth <= 767) {
+        boxWidth = box.clientWidth / 8
+      }
+      this.boxWidth.width = `${boxWidth}px`
+    }
+  },
+  mounted () {
     const { width } = useWindowSize()
     this.widthSize = width
     this.getNewProduct()
-  },
-  mounted () {
     const box = this.$refs.ProductBox
-    console.log(box.clientWidth)
-    const boxWidth = (box.clientWidth / 8) * 4
-    this.boxWidth.width = `${boxWidth}px`
+    this.step = box.clientWidth / 8
   }
 }
 </script>
@@ -136,6 +127,7 @@ export default {
   left: -30px;
   background-color: rgba(255, 255, 0, 1)
 }
+@media (max-width: 1199px) {}
 @media (max-width: 991px){
   .blockOut{
     overflow: hidden;
