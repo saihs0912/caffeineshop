fc43b4e423ab61dfe93963c67f40c1fcc14e304d
saihs0912 <h71534@gmail.com>
2025-11-01 02:09:02 +0800

    繼續更新商品頁功能

--- src/components/front/RelatedProducts.vue
+++ src/components/front/RelatedProducts.vue
@@ -48,13 +48,18 @@ export default {
           this.ProductList = this.ProductList.filter(item => {
             return (item.category === this.sendProduct.category) && (item.id !== this.sendProduct.id)
           }).reverse()
-          console.log('執行了這邊')
           let boxWidth = ''
           let boxInsideWidth = ''
           this.num = this.ProductList.length
           boxInsideWidth = this.step * this.num
           if (this.widthSize >= 1200) {
-            boxWidth = (boxInsideWidth / this.num) * 4
+            this.num > 5 ? boxWidth = (boxInsideWidth / this.num) * 5 : boxWidth = this.step * this.num
+          } else if (this.widthSize <= 1199 && this.widthSize >= 992) {
+            this.num > 4 ? boxWidth = (boxInsideWidth / this.num) * 4 : boxWidth = this.step * this.num
+          } else if (this.widthSize <= 991 && this.widthSize >= 768) {
+            this.num > 4 ? boxWidth = (boxInsideWidth / this.num) * 3 : boxWidth = this.step * this.num
+          } else if (this.widthSize <= 767) {
+            this.num > 4 ? boxWidth = (boxInsideWidth / this.num) * 1 : boxWidth = this.step * this.num
           }
           this.boxInsideWidth.width = `${boxInsideWidth}px`
           this.boxWidth.width = `${boxWidth}px`
@@ -62,28 +67,37 @@ export default {
     }
   },
   watch: {
-    sendProduct () {
-      this.getNewProducts()
+    sendProduct: {
+      handler (newVal) {
+        if (newVal && newVal.id) {
+          this.getNewProducts()
+        }
+      },
+      immediate: true,
+      deep: false
     },
     widthSize (newWidth, oldWidth) {
-      const box = this.$refs.ProductListBox
-      let boxWidth = ''
-      if (newWidth >= 1200) {
-        boxWidth = (box.clientWidth / this.ProductList.length) * 3
-      } else if (newWidth <= 1199 && newWidth >= 992) {
-        boxWidth = (box.clientWidth / 8) * 3
-      } else if (newWidth <= 991 && newWidth >= 768) {
-        boxWidth = (box.clientWidth / 8) * 2
-      } else if (newWidth <= 767) {
-        boxWidth = box.clientWidth / 8
+      if (oldWidth) {
+        this.num = this.ProductList.length
+        const box = this.$refs.ProductListBox
+        let boxWidth = 0
+        if (newWidth >= 1200) {
+          this.num > 5 ? boxWidth = (box.clientWidth / this.num) * 5 : boxWidth = this.step * this.num
+        } else if (newWidth <= 1199 && newWidth >= 992) {
+          this.num > 4 ? boxWidth = (box.clientWidth / this.num) * 4 : boxWidth = this.step * this.num
+        } else if (newWidth <= 991 && newWidth >= 768) {
+          this.num > 4 ? boxWidth = (box.clientWidth / this.num) * 3 : boxWidth = this.step * this.num
+        } else if (newWidth <= 767) {
+          boxWidth = box.clientWidth / this.num
+        }
+        this.boxWidth.width = `${Math.floor(boxWidth)}px`
       }
-      this.boxWidth.width = `${boxWidth}px`
     }
   },
   mounted () {
     const { width } = useWindowSize()
     this.widthSize = width
-    this.getNewProducts()
+    // this.getNewProducts()
   }
 }
 </script>
