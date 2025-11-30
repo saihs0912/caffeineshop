26583cf115d72fb70520be9acf25addc5ed48f2d
saihs0912 <h71534@gmail.com>
2025-12-01 01:08:52 +0800

    更新404

--- src/views/front/NotFound.vue
+++ src/views/front/NotFound.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container" style="max-width: 600px; height: 100vh">
-    <div class="row align-items-center h-100 align-content-center">
+  <div class="container" style="max-width: 600px;">
+    <div class="row align-items-center align-content-center mt-4 mb-4" style="min-height: calc(100vh - 332px);">
       <div class="col-12 text-center">
         <h1>Oops...頁面不存在</h1>
         <p class="fs-5">將於 10 秒後跳回首頁</p>
       </div>
-      <div class="col-12mx-auto" style="filter: grayscale(100%)">
+      <div class="col-lg-4 col-md-4 col-sm-6 col-9 mx-auto" style="filter: grayscale(100%)">
         <img src="../../assets/img/404.jpg" alt="Oops...頁面不存在" class="img-fluid" />
       </div>
     </div>
