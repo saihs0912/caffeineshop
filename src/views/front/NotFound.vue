30d38b3116ff7f4f09a0630c5fae82cf6ceb2ec8
saihs0912 <h71534@gmail.com>
2025-12-17 00:24:08 +0800

    更新404頁面

--- src/views/front/NotFound.vue
+++ src/views/front/NotFound.vue
@@ -3,7 +3,7 @@
     <div class="row align-items-center align-content-center mt-4 mb-4" style="min-height: calc(100vh - 332px);">
       <div class="col-12 text-center">
         <h1>Oops...頁面不存在</h1>
-        <p class="fs-5">將於 10 秒後跳回首頁</p>
+        <p class="fs-5">將於 {{ num }} 秒後跳回首頁</p>
       </div>
       <div class="col-lg-4 col-md-4 col-sm-6 col-9 mx-auto" style="filter: grayscale(100%)">
         <img src="../../assets/img/404.jpg" alt="Oops...頁面不存在" class="img-fluid" />
@@ -14,7 +14,32 @@
 
 <script>
 export default {
-  name: 'NotFound'
+  name: 'NotFound',
+  data () {
+    return {
+      num: 10
+    }
+  },
+  methods: {
+    countDown () {
+      const count = setInterval(() => {
+        this.num --
+        if (this.num === 0) clearInterval(count)
+      }, 1000)
+    }
+  },
+  watch: {
+    num (newNum, oldNum) {
+      if (newNum === 0) {
+        setTimeout(() => {
+          this.$router.push('/')
+        }, 1000)
+      }
+    }
+  },
+  mounted () {
+    this.countDown()
+  }
 }
 </script>
 
