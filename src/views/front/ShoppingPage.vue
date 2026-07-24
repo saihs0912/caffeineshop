a4a6ff9ad2f70861b7118f7e2984b19d0ba4d969
saihs0912 <h71534@taitra.org.tw>
2026-07-24 18:02:54 +0800

    更新麵包屑

--- src/views/front/ShoppingPage.vue
+++ src/views/front/ShoppingPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container pt-4">
     <div class="row">
+      <div class="col-12">
+        <bread-crumb :title="'線上商店'"></bread-crumb>
+      </div>
       <div class="col-lg-3 col-md-3 col-sm-12 col-12 order-md-first order-lg-first">
         <side-menu />
       </div>
@@ -18,11 +21,11 @@ import SideMenu from '@/components/front/SideMenu.vue'
 import ShoppingList from '@/components/front/ShoppingList.vue'
 import OrderAndSearch from '@/components/front/OrderAndSearch.vue'
 import FrontPagination from '@/components/front/FrontPagination.vue'
+import BreadCrumb from '@/components/front/BreadCrumb.vue'
 
 export default {
   head() {
     return {
-      title: '商品列表 - 咖啡因商店',
       meta: [
         { name: 'description', content: '一間販賣咖啡因商品的網路店鋪' },
         { name: 'keywords', content: '咖啡因商店,咖啡豆,濾掛式咖啡,茶葉,茶包,甜點' }
@@ -33,7 +36,11 @@ export default {
     SideMenu,
     ShoppingList,
     OrderAndSearch,
-    FrontPagination
+    FrontPagination,
+    BreadCrumb
+  },
+  mounted() {
+    console.log(this.$route.meta.title)
   }
 }
 </script>
